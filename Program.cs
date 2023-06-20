a1e3ea5fc6ed81a1827ecc53e2c7b2ce57b9a85d
Morozova Natasha <olodang@gmail.com>
2023-06-20 10:39:52 +0300

    задача выполнена

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿Console.WriteLine("Введите строки через зпятую");
 string[] words = Console.ReadLine().Split(",");
+for(int i=0; i<words.Length; i++)
+{
+    if(words[i].Length<=3)
+        Console.WriteLine(words[i]);
+}
