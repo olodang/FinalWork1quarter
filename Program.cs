a9580ebccf6129d0cbee54fcd00c6ff2bc3597de
Morozova Natasha <olodang@gmail.com>
2023-06-20 10:38:25 +0300

    приняли строки

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите строки через зпятую");
+string[] words = Console.ReadLine().Split(",");
